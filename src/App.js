05a0eece60113b32a947960cd913a6f7bc459a8e
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2021-06-04 17:07:42 +0700

    feat(base): done qr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import QrCodeWithLogo from 'qrcode-with-logos'
 
-function App() {
+const fileToDataUri = (file) => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    resolve(event.target.result)
+  };
+  reader.readAsDataURL(file);
+})
+
+const App = () => {
+  const [url, setUrl] = useState(0);
+  const [urlImage, setUrlImage] = useState(0);
+  const [file, setFile] = useState({})
+  const handleGenerate = () => {
+    const date = new Date().getTime().toString()
+    const qr = new QrCodeWithLogo({
+      canvas: document.getElementById("canvas"),
+      content: url,
+      width: 380,
+      downloadName: `xxidbr9_qr_code_${date}`,
+      download: true,
+      image: document.getElementById("image"),
+      logo: {
+        src: urlImage
+      }
+    }).toImage();
+    qr.then(e => console.log(e)).catch(console.log)
+  }
+
+  useEffect(() => {
+    if (!file) {
+      setUrlImage('');
+      return;
+    }
+    if (!!file.name) {
+      fileToDataUri(file)
+        .then(setUrlImage)
+    }
+  }, [file])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-pink-400">
+      <div className="container max-w-screen-md px-5 mx-auto text-center bg-yellow-400">
+        <div>
+          <input className="mb-4 w-full bg-gray-100 px-4 py-2" type="text" onChange={e => setUrl(e.target.value)} placeholder="masukan url" />
+        </div>
+        <div>
+          <input className="w-full" type="file" onChange={e => setFile(e.target.files[0])} />
+        </div>
+        <div>
+          <canvas id="canvas" style={{ display: "none" }}></canvas>
+          <img src="" alt="" id="image" className="w-full" />
+        </div>
+        <div className="w-max">
+          <button onClick={handleGenerate} className="bg-purple-500 w-full ">Generate QR</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
